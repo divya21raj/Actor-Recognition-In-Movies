8de58970618b68dfa32bf1c325423d63071f04f7
Divya Raj <divya21raj@gmail.com>
2020-08-18 09:44:23 +0530

    Cleanup

--- faceRecImage.py
+++ faceRecImage.py
@@ -1,20 +1,22 @@
-import face_recognition
 import argparse
 import pickle
+
 import cv2
-from sklearn.neighbors import KDTree
+import face_recognition
 import numpy as np
+from sklearn.neighbors import KDTree
 
 import constants
 
-def linear_search(data,query_encodings):
+
+def linear_search(data, query_encodings):
     names = []
     # loop over the facial embeddings
     for encoding in query_encodings:
         # attempt to match each face in the input image to our known
         # encodings
         matches = face_recognition.compare_faces(data[constants.ENCODINGS],
-            encoding)
+                                                 encoding)
         name = constants.ID_UNKNOWN
 
         # check to see if we have found a match
@@ -40,24 +42,26 @@ def linear_search(data,query_encodings):
         names.append(name)
     return names
 
+
 # find the best match for the given set of query encodings with given tolerance for distance value
 # returns then names of matched actors or constants.ID_UNKNOWN in case of no valid match with tolerance
-def find_best_match_within_tolerance(candidates, names, tolerance): 
+def find_best_match_within_tolerance(candidates, names, tolerance):
     zipped_dist_names = np.dstack(candidates)
     best_candidates = []
-    
-    for candidates in zipped_dist_names: 
+
+    for candidates in zipped_dist_names:
         count = {}
         best_candidate = constants.ID_UNKNOWN
         filtered_candidates = [int(ind) for dist, ind in candidates if dist <= tolerance]
-        
+
         if len(filtered_candidates) != 0:
             for ind in filtered_candidates:
                 count[names[ind]] = count.get(names[ind], 0) + 1
-            best_candidate = max(count, key = count.get)
-        
+            best_candidate = max(count, key=count.get)
+
         best_candidates.append(best_candidate)
     return best_candidates
+
 
 # find the k nearest neighbors using precomputed kdtree of training encodings
 # return the names of most face for query or constants.ID_UNKNOWN in case of no valid match
@@ -70,45 +74,45 @@ def fast_face_match_knn(data, query_encodings, tolerance, k):
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-e", "--encodings", required=True,
-    help="path to serialized db of facial encodings")
+                help="path to serialized db of facial encodings")
 ap.add_argument("-i", "--image", required=True,
-    help="path to input image")
+                help="path to input image")
 ap.add_argument("-d", "--detection-method", type=str, default="cnn",
-    help="face detection model to use: either `hog` or `cnn`")
-ap.add_argument("-fnn","--fast-nn",action="store_true")
+                help="face detection model to use: either `hog` or `cnn`")
+ap.add_argument("-fnn", "--fast-nn", action="store_true")
 args = vars(ap.parse_args())
 print(args)
 
 # load the known faces and embeddings
 print("[INFO] loading encodings...")
 data = pickle.loads(open(args["encodings"], "rb").read())
- 
+
 # load and convert the image from BGR color (which OpenCV uses) 
 # to RGB color (which face_recognition uses)
 image = cv2.imread(args["image"])
 rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
- 
+
 # detect the (x, y)-coordinates of the bounding boxes corresponding
 # to each face in the input image, then compute the facial embeddings
 # for each face
 print("[INFO] recognizing faces...")
 boxes = face_recognition.face_locations(rgb,
-    model=args["detection_method"])
+                                        model=args["detection_method"])
 encodings = face_recognition.face_encodings(rgb, boxes)
 
 # based on user args select fast kdtree based nn or linear seach
 names = []
 if args["fast_nn"] or data[constants.ENCODING_STRUCTURE] == constants.ENC_KDTREE:
     # check if kdtree is to be recomputed or not
-    if data[constants.ENCODING_STRUCTURE] != constants.ENC_KDTREE :
+    if data[constants.ENCODING_STRUCTURE] != constants.ENC_KDTREE:
         data[constants.ENCODINGS] = KDTree(np.asarray(data[constants.ENCODINGS]),
                                            leaf_size=constants.LEAF_SIZE_KDTREE)
         data[constants.ENCODING_STRUCTURE] = constants.ENC_KDTREE
-    
+
     names = fast_face_match_knn(data, encodings, constants.NORM_DIST_TOLERANCE, constants.K_NN)
 else:
-    names = linear_search(data,encodings)
-print("names : ",names)
+    names = linear_search(data, encodings)
+print("names : ", names)
 
 # loop over the recognized faces
 for ((top, right, bottom, left), name) in zip(boxes, names):
@@ -116,8 +120,8 @@ for ((top, right, bottom, left), name) in zip(boxes, names):
     cv2.rectangle(image, (left, top), (right, bottom), (0, 255, 0), 2)
     y = top - 15 if top - 15 > 15 else top + 15
     cv2.putText(image, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
-        0.75, (0, 255, 0), 2)
- 
+                0.75, (0, 255, 0), 2)
+
 # show the output image
 cv2.imshow("Image", image)
 cv2.waitKey(0)
--- faceRecVideo.py
+++ faceRecVideo.py
@@ -1,22 +1,24 @@
-from imutils.video import VideoStream
-import face_recognition
 import argparse
-import imutils
 import pickle
 import time
+
 import cv2
+import face_recognition
+import imutils
 import numpy as np
+from imutils.video import VideoStream
 
 import constants
 
-def linear_search(data,query_encodings):
+
+def linear_search(data, query_encodings):
     names = []
     # loop over the facial embeddings
     for encoding in query_encodings:
         # attempt to match each face in the input image to our known
         # encodings
         matches = face_recognition.compare_faces(data[constants.ENCODINGS],
-            encoding)
+                                                 encoding)
         name = constants.ID_UNKNOWN
 
         # check to see if we have found a match
@@ -42,24 +44,26 @@ def linear_search(data,query_encodings):
         names.append(name)
     return names
 
+
 # find the best match for the given set of query encodings with given tolerance for distance value
 # returns then names of matched actors or constants.ID_UNKNOWN in case of no valid match with tolerance
-def find_best_match_within_tolerance(candidates, names, tolerance): 
+def find_best_match_within_tolerance(candidates, names, tolerance):
     zipped_dist_names = np.dstack(candidates)
     best_candidates = []
-    
-    for candidates in zipped_dist_names: 
+
+    for candidates in zipped_dist_names:
         count = {}
         best_candidate = constants.ID_UNKNOWN
         filtered_candidates = [int(ind) for dist, ind in candidates if dist <= tolerance]
-        
+
         if len(filtered_candidates) != 0:
             for ind in filtered_candidates:
                 count[names[ind]] = count.get(names[ind], 0) + 1
-            best_candidate = max(count, key = count.get)
-        
+            best_candidate = max(count, key=count.get)
+
         best_candidates.append(best_candidate)
     return best_candidates
+
 
 # find the k nearest neighbors using precomputed kdtree of training encodings
 # return the names of most face for query or constants.ID_UNKNOWN in case of no valid match
@@ -72,21 +76,21 @@ def fast_face_match_knn(data, query_encodings, tolerance, k):
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-e", "--encodings", required=True,
-    help="path to serialized db of facial encodings")
+                help="path to serialized db of facial encodings")
 ap.add_argument("-o", "--output", type=str,
-    help="path to output video")
+                help="path to output video")
 ap.add_argument("-y", "--display", type=int, default=1,
-    help="whether or not to display output frame to screen")
+                help="whether or not to display output frame to screen")
 ap.add_argument("-d", "--detection-method", type=str, default="cnn",
-    help="face detection model to use: either `hog` or `cnn`")
-ap.add_argument("-fnn","--fast-nn",action="store_true")
+                help="face detection model to use: either `hog` or `cnn`")
+ap.add_argument("-fnn", "--fast-nn", action="store_true")
 args = vars(ap.parse_args())
 print(args)
 
 # load the known faces and embeddings
 print("[INFO] loading encodings...")
 data = pickle.loads(open(args["encodings"], "rb").read())
- 
+
 # initialize the video stream and pointer to output video file, then
 # allow the camera sensor to warm up
 print("[INFO] starting video stream...")
@@ -98,34 +102,34 @@ time.sleep(2.0)
 while True:
     # grab the frame from the threaded video stream
     frame = vs.read()
-    
+
     # convert the input frame from BGR to RGB then resize it to have
     # a width of 750px (to speedup processing)
     rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     rgb = imutils.resize(frame, width=750)
     r = frame.shape[1] / float(rgb.shape[1])
- 
+
     # detect the (x, y)-coordinates of the bounding boxes
     # corresponding to each face in the input frame, then compute
     # the facial embeddings for each face
     boxes = face_recognition.face_locations(rgb,
-        model=args["detection_method"])
+                                            model=args["detection_method"])
     encodings = face_recognition.face_encodings(rgb, boxes)
 
     # based on user args select fast kdtree based nn or linear seach
     names = []
-    if encodings :
+    if encodings:
         if args["fast_nn"] or data[constants.ENCODING_STRUCTURE] == constants.ENC_KDTREE:
             # check if kdtree is to be recomputed or not
-            if data[constants.ENCODING_STRUCTURE] != constants.ENC_KDTREE :
+            if data[constants.ENCODING_STRUCTURE] != constants.ENC_KDTREE:
                 data[constants.ENCODINGS] = KDTree(np.asarray(data[constants.ENCODINGS]),
-                                                leaf_size=constants.LEAF_SIZE_KDTREE)
+                                                   leaf_size=constants.LEAF_SIZE_KDTREE)
                 data[constants.ENCODING_STRUCTURE] = constants.ENC_KDTREE
-            
+
             names = fast_face_match_knn(data, encodings, constants.NORM_DIST_TOLERANCE, constants.K_NN)
         else:
-            names = linear_search(data,encodings)
-        #print("names : ",names)
+            names = linear_search(data, encodings)
+        # print("names : ",names)
 
     # loop over the recognized faces
     for ((top, right, bottom, left), name) in zip(boxes, names):
@@ -134,21 +138,21 @@ while True:
         right = int(right * r)
         bottom = int(bottom * r)
         left = int(left * r)
- 
+
         # draw the predicted face name on the image
         cv2.rectangle(frame, (left, top), (right, bottom),
-            (0, 255, 0), 2)
+                      (0, 255, 0), 2)
         y = top - 15 if top - 15 > 15 else top + 15
         cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
-            0.75, (0, 255, 0), 2)
+                    0.75, (0, 255, 0), 2)
 
     # if the video writer is None *AND* we are supposed to write
     # the output video to disk initialize the writer
     if writer is None and args["output"] is not None:
         fourcc = cv2.VideoWriter_fourcc(*"MJPG")
         writer = cv2.VideoWriter(args["output"], fourcc, 20,
-            (frame.shape[1], frame.shape[0]), True)
- 
+                                 (frame.shape[1], frame.shape[0]), True)
+
     # if the writer is not None, write the frame with recognized
     # faces to disk
     if writer is not None:
@@ -159,7 +163,7 @@ while True:
     if args["display"] > 0:
         cv2.imshow("Frame", frame)
         key = cv2.waitKey(1) & 0xFF
- 
+
         # if the `q` key was pressed, break from the loop
         if key == ord("q"):
             break
@@ -167,7 +171,7 @@ while True:
 # Cleanup
 cv2.destroyAllWindows()
 vs.stop()
- 
+
 # check to see if the video writer point needs to be released
 if writer is not None:
     writer.release()
